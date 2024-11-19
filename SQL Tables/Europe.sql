3bc5b070c91eb1789315ed7ba9089bd166e9d3dc
Ayemhenre Isikhuemhen <127813264+Taotlema@users.noreply.github.com>
2024-11-19 11:35:06 -0500

    Commenting Update

---
+++ SQL Tables/Europe.sql
@@ -0,0 +1,9 @@
+/*
+ITCS-3169-Final-Project
+November 19, 2024
+Author(s): Ayemhenre Isikhuemhen,  
+*/
+
+-- create table
+
+-- load records
---
+++ SQL Tables/Queries.sql
@@ -0,0 +1,48 @@
+/*
+Query 1: 
+Author: 
+Descirption:
+*/
+
+/*
+Query 2: 
+Author:
+Descirption:
+*/
+
+/*
+Query 3: 
+Author:
+Descirption:
+*/
+
+/*
+Query 4: 
+Author:
+Descirption:
+*/
+
+/*
+Query 5: 
+Author:
+Descirption:
+*/
+
+
+/*
+Join Query 1: 
+Author:
+Descirption:
+*/
+
+/*
+Join Query 2: 
+Author:
+Descirption:
+*/
+
+/*
+Join Query 3: 
+Author:
+Descirption:
+*/
