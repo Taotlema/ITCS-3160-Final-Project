c1f611e2e50aa228a506361f1176ac94ef32d70f
aweyandt <aweyandt@charlotte.edu>
2024-11-28 14:36:46 -0500

    Renamed for clarity

--- SQL Tables/Exports_23.sql
+++ SQL Tables/Exports_23.sql
@@ -6,7 +6,7 @@ Author(s): Ayemhenre Isikhuemhen, Nathalie Brown
 
 -- create table
 
-CREATE TABLE EXPORTS_23 (
+CREATE TABLE EXPORTS_Total_23 (
     country VARCHAR(20) PRIMARY KEY,
     SEP_23 INT,
     AUG_23 INT,
@@ -17,25 +17,25 @@ CREATE TABLE EXPORTS_23 (
 
 -- load records
 
-INSERT INTO EXPORTS_23 values('Austria', 383, 419, 4211);
-INSERT INTO EXPORTS_23 values('Belgium', 2808, 2712, 30727);
-INSERT INTO EXPORTS_23 values('Czech Republic', 382, 534, 3447);
-INSERT INTO EXPORTS_23 values('Finland',192, 233, 2339);
-INSERT INTO EXPORTS_23 values('France', 3561, 3516, 32180);
-INSERT INTO EXPORTS_23 values('Germany', 6743, 6799, 58074);
-INSERT INTO EXPORTS_23 values('Hungary', 288, 267, 2255);
-INSERT INTO EXPORTS_23 values('Ireland', 1123, 1381, 12531);
-INSERT INTO EXPORTS_23 values('Italy', 3240, 2595, 21349);
-INSERT INTO EXPORTS_23 values('Netherlands', 6262, 6825, 59642);
-INSERT INTO EXPORTS_23 values('Poland', 942, 934, 8162);
-INSERT INTO EXPORTS_23 values('Austrailia', 2868, 2863, 24971);
-INSERT INTO EXPORTS_23 values('China', 11796, 10765, 106000);
-INSERT INTO EXPORTS_23 values('Hong Kong', 3561, 2295, 21151);
-INSERT INTO EXPORTS_23 values('Indonesia', 769, 873, 7269);
-INSERT INTO EXPORTS_23 values('Japan', 6627, 6510, 56642);
-INSERT INTO EXPORTS_23 values('Korea_South', 5918, 5493, 47439);
-INSERT INTO EXPORTS_23 values('Malaysia', 1736, 1705, 13577);
-INSERT INTO EXPORTS_23 values('Philiphines', 762, 793, 6721);
-INSERT INTO EXPORTS_23 values('Singapore', 4108, 4101, 31452);
-INSERT INTO EXPORTS_23 values('Taiwan', 2874, 3589, 29693);
+INSERT INTO EXPORTS_Total_23 values('Austria', 383, 419, 4211);
+INSERT INTO EXPORTS_Total_23 values('Belgium', 2808, 2712, 30727);
+INSERT INTO EXPORTS_Total_23 values('Czech Republic', 382, 534, 3447);
+INSERT INTO EXPORTS_Total_23 values('Finland',192, 233, 2339);
+INSERT INTO EXPORTS_Total_23 values('France', 3561, 3516, 32180);
+INSERT INTO EXPORTS_Total_23 values('Germany', 6743, 6799, 58074);
+INSERT INTO EXPORTS_Total_23 values('Hungary', 288, 267, 2255);
+INSERT INTO EXPORTS_Total_23 values('Ireland', 1123, 1381, 12531);
+INSERT INTO EXPORTS_Total_23 values('Italy', 3240, 2595, 21349);
+INSERT INTO EXPORTS_Total_23 values('Netherlands', 6262, 6825, 59642);
+INSERT INTO EXPORTS_Total_23 values('Poland', 942, 934, 8162);
+INSERT INTO EXPORTS_Total_23 values('Austrailia', 2868, 2863, 24971);
+INSERT INTO EXPORTS_Total_23 values('China', 11796, 10765, 106000);
+INSERT INTO EXPORTS_Total_23 values('Hong Kong', 3561, 2295, 21151);
+INSERT INTO EXPORTS_Total_23 values('Indonesia', 769, 873, 7269);
+INSERT INTO EXPORTS_Total_23 values('Japan', 6627, 6510, 56642);
+INSERT INTO EXPORTS_Total_23 values('Korea_South', 5918, 5493, 47439);
+INSERT INTO EXPORTS_Total_23 values('Malaysia', 1736, 1705, 13577);
+INSERT INTO EXPORTS_Total_23 values('Philiphines', 762, 793, 6721);
+INSERT INTO EXPORTS_Total_23 values('Singapore', 4108, 4101, 31452);
+INSERT INTO EXPORTS_Total_23 values('Taiwan', 2874, 3589, 29693);
 
